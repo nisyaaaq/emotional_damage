bbf2b8c83285f2464c7086956c223fcc6b98f88e
nisyaaaq <52811308+nisyaaaq@users.noreply.github.com>
2022-06-07 21:02:28 +0800

    Add files via upload

---
+++ solutions/Payment.java
@@ -0,0 +1,191 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package keLima;
+
+import java.util.*;
+
+/**
+ *
+ * @author nisyaqanita
+ */
+public class Payment implements Comparable<Payment> {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private Long epoch;
+    private String txnId;
+    private String tier;
+    
+    final static String PLATINUM = "PLATINUM";
+    final static String GOLD = "GOLD";
+    final static String SILVER = "SILVER";
+    final static String BRONZE = "BRONZE";
+    
+    public Payment(Long epoch, String txnId, String tier) {
+        
+//        decrease epoch time regarding the tier
+        if (tier.equalsIgnoreCase(PLATINUM)) {
+            epoch -= 3000;
+        }
+        else if (tier.equalsIgnoreCase(GOLD)) {
+            epoch -= 2000;
+        }
+        else if (tier.equalsIgnoreCase(SILVER)) {
+            epoch -= 1000;
+        } 
+        else if (tier.equalsIgnoreCase(BRONZE)) {
+            epoch -= 0;
+        } 
+        else {
+            throw new InputMismatchException();
+        }
+        
+        this.epoch = epoch;
+        this.txnId = txnId;
+        this.tier = tier;
+    }
+
+    public Long getEpoch() {
+        return epoch;
+    }
+
+    public void setEpoch(Long epoch) {
+        this.epoch = epoch;
+    }
+
+    public String getTxnId() {
+        return txnId;
+    }
+
+    public void setTxnId(String txnId) {
+        this.txnId = txnId;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
+    
+    @Override
+    public int compareTo(Payment o) {
+        return this.getEpoch().compareTo(o.getEpoch());
+    }
+    
+    public static String toStr(PriorityQueue<Payment> q) {
+        String result = "";
+        int i = 0; // **
+        while(!q.isEmpty() && i < 100){ // **
+            result += q.poll() + " ";
+            i++; //**
+        }
+//        System.out.println("queue's current length: " + q.size());
+        return result.trim(); // added .trim() method
+    }
+    
+    @Override
+    public String toString() {
+        return this.getTxnId();
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final String exit = "EXIT";
+        final String reboot = "REBOOT";
+        
+        Long epoch;
+        Long lastAddedEpoch = 0L;
+        String txnId;
+        String tier;
+//        String [] inArr = new String[3]; // added just now
+        int diff = 0;
+        
+        PriorityQueue<Payment> meowsQ = new PriorityQueue<>(/*Collections.reverseOrder()*/);
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNextLine()) {
+            
+            try {
+                String in = sc.nextLine();
+                in.trim();
+
+                if (in.equalsIgnoreCase(exit)) {
+                    //print txnId
+//                    System.out.println(toStr(meowsQ)); // **
+                    break;
+                } else if (in.equalsIgnoreCase(reboot)) {
+                    meowsQ.clear(); 
+//                    break; // ** it got commented
+                } else {
+                    String [] inArr = in.split(" ", 3); // deleted 'String[]' just now
+                        // i commented this exception
+//                    if (inArr.length != 3 || inArr.length != 1) {
+//                        throw new InputMismatchException();
+//                    }
+                    int length = inArr[0].length();
+                    if (length != 13) {
+                        throw new InputMismatchException();
+                    }
+                    epoch = Long.valueOf(inArr[0]);
+                    txnId = inArr[1];
+                    tier = inArr[2];
+
+                    // decide if a second has elapsed
+                    if (!meowsQ.isEmpty()) {
+                        // peek to last epoch added to the queue
+//                        Long lastTransaction = meowsQ.peek().getEpoch();
+//                        String tierChecker = meowsQ.peek().getTier();
+
+//                        if (tierChecker.equalsIgnoreCase(PLATINUM)) {
+//                            lastAddedEpoch += 3000;
+//                        } else if (tierChecker.equalsIgnoreCase(GOLD)) {
+//                            lastAddedEpoch += 2000;
+//                        } else if (tierChecker.equalsIgnoreCase(SILVER)) {
+//                            lastAddedEpoch += 1000;
+//                        }
+
+                        lastAddedEpoch /= 1000;
+                        int secondDigitLast = (lastAddedEpoch.intValue()) % 10;
+
+                        // take the digit seconds from just added transaction
+                        Long epochTemp = epoch;
+                        epochTemp /= 1000;
+                        int secondDigitJust = (epochTemp.intValue()) % 10;
+                        
+                        diff = secondDigitJust - secondDigitLast;
+                        lastAddedEpoch = epoch;
+                    } else {
+                        lastAddedEpoch = epoch;
+                    }
+                    meowsQ.add(new Payment(epoch, txnId, tier));
+                    if (diff == 1) { 
+                        //print txnId
+                        System.out.println(toStr(meowsQ));
+//                        System.out.println();
+                        diff = 0; // just added this line
+//                        break; // got commented
+                    }
+                }
+            } catch (NumberFormatException e) {
+//                System.out.println("num form exp");
+                return;
+            } catch (InputMismatchException e) {
+//                System.out.println("in mis exp");
+                return;
+            } catch (ArrayIndexOutOfBoundsException e) {
+//                System.out.println("arr ind out of bnd");
+                return;
+            }
+        }          
+  
+    }
+
+    
+    
+}
