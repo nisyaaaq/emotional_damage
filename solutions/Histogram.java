2d2f6383f6e9ebb4d81506ec2015a15b98cb6764
Sanjivan28 <93278286+Sanjivan28@users.noreply.github.com>
2022-06-21 17:45:10 +0800

    Update Histogram.java

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -1,84 +1,49 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
-class Histogram{
 
-    public static void main(String[] args){
+public class Histogram {
 
-        Scanner in = new Scanner(System.in);
-        int NumOfCases = in.nextInt();
-//        ArrayList<Integer> ans = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        for(int c=0; c<NumOfCases; c++) {
-            int NumOfData = in.nextInt();
-            int NumOfBins = in.nextInt();
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int numOfDataPoints = scanner.nextInt();
+            int numOfBins = scanner.nextInt();
 
-            int[] DataPoints = new int[NumOfData];
-            for (int b = 0; b < NumOfData; b++) {
-                DataPoints[b] = in.nextInt();
+            int[] dataPoints = new int[numOfDataPoints];
+            for (int j = 0; j < numOfDataPoints; j++) {
+                dataPoints[j] = scanner.nextInt();
             }
 
-            int max = DataPoints[0];
-            int min = DataPoints[0];
+            int max = Arrays.stream(dataPoints).max().getAsInt();
+            int min = Arrays.stream(dataPoints).min().getAsInt();
 
-            for (int i = 0; i < DataPoints.length; i++) {
-                if (DataPoints[i] > max) {
-                    max = DataPoints[i];
-                }
-                if (DataPoints[i] < min) {
-                    min = DataPoints[i];
-                }
+            int[] cutoffs = new int[numOfBins + 1];
+            int[] binsCount = new int[numOfBins];
+            int interval = (max - min) / numOfBins;
+            for (int j = 0; j < numOfBins + 1; j++) {
+                cutoffs[j] = min + (interval * j);
             }
 
-            int CutoffsValue = (max - min) / NumOfBins;
-            int sum = 0;
-            int[] interval = new int[NumOfBins + 1];
-            int[] count = new int[NumOfBins];
-
-            for (int i = 0; i < interval.length; i++) {
-                if (i == 0) {
-                    System.out.print(min + " ");
-                    sum += min;
-                    interval[i] = min;
-//                    ans.add(min);
-                } else {
-                    System.out.print(sum + " ");
-                    interval[i] = sum;
-//                    ans.add(sum);
-                }
-                sum += CutoffsValue;
-            }
-//            ans.add(00);
-            System.out.println();
-
-
-            for (int i = 0; i < DataPoints.length; i++) {
-                for (int j = 0; j < interval.length - 1; j++) {
-                    if (DataPoints[i] >= interval[j] && DataPoints[i] < interval[j + 1]) {
-                        count[j]++;
+            dataPointsLoop:
+            for (int data: dataPoints) {
+                for (int k = 0; k < cutoffs.length - 2; k++) {
+                    if(data < cutoffs[k + 1]){
+                        binsCount[k]++;
+                        continue dataPointsLoop;
                     }
                 }
-                if (DataPoints[i] == interval[interval.length - 1]) {
-                    count[interval.length - 2]++;
+                if(data <= cutoffs[cutoffs.length - 1]){
+                    binsCount[numOfBins - 1]++;
                 }
             }
-//            for (int i = 0; i < count.length; i++){
-//                ans.add(count[i]);
-//            }
-//            ans.add(00);
 
-            for (int i = 0; i < count.length; i++) {
-                System.out.print(count[i] + " ");
-            }
-            System.out.println();
+            System.out.println(Arrays.toString(cutoffs).replace("[", "").replace("]", "").replace(",", ""));
+            System.out.println(Arrays.toString(binsCount).replace("[", "").replace("]", "").replace(",", ""));
         }
-
-//        while(!ans.isEmpty()){
-//            int num = ans.remove(0);
-//            if(num != 0){
-//                System.out.print(num + " ");
-//            }
-//            if(num == 0){
-//                System.out.println();
-//            }
-//        }
     }
+
 }
