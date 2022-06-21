af482ca31979ef36b7047997451655c44c7accee
Sanjivan28 <93278286+Sanjivan28@users.noreply.github.com>
2022-06-21 13:31:20 +0800

    Update Histogram.java

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -1,80 +1,49 @@
-import java.io.File;
 import java.io.FileInputStream;
-import java.util.*;
-import java.lang.*;
-/**
- *
- */
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Histogram {
+
     public static void main(String[] args) {
-        try {
-            Scanner input = new Scanner(System.in);
-            String firstLine = input.nextLine();
-            System.out.println(firstLine);
-            int numOfBin = Integer.parseInt(firstLine.split(" ")[1]);
-            String secondLine = input.nextLine();
-            String[] strArr2 = secondLine.split(" ");
-            int[] N2 = new int[strArr2.length];
-            for (int i = 0; i < N2.length; i++) {
-                N2[i] = Integer.parseInt(strArr2[i]);
-            }
-            int[] newArray = new int[numOfBin + 1];;
-            int cutoff = intervals(N2, numOfBin, newArray);
-            Frequency(N2,numOfBin,newArray);
+        Scanner scanner = new Scanner(System.in);
 
-        } catch (Exception e) {
-            return;
-        }
-    }
-    public static int max(int[] array){
-            int max = 0;
-            for(int i=0;i< array.length;i++){
-                if(array[i]>max){
-                    max = array[i];
-                }
-            }
-            return max;
-        }
-        public static int min(int[] array){
-            int min = array[0];
-            for(int i=0;i< array.length; i++){
-                if(array[i]<min){
-                    min = array[i];
-                }
-            }
-            return min;
-        }
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int numOfDataPoints = scanner.nextInt();
+            int numOfBins = scanner.nextInt();
 
-        public static int intervals(int[] N2, int numOfBin, int[] newArray){
-
-            int min = min(N2);
-            int max = max(N2);
-            int cutoff = (max - min) / numOfBin;
-            int interval = min;
-            for (int i = 0; i <= numOfBin; i++) {
-                newArray[i] = interval;
-                interval += cutoff;
-
+            int[] dataPoints = new int[numOfDataPoints];
+            for (int j = 0; j < numOfDataPoints; j++) {
+                dataPoints[j] = scanner.nextInt();
             }
-            for (int inter: newArray) {
-                System.out.print(inter + " ");
-            }
-            System.out.println();
-            return cutoff;
-        }
 
-        public static void Frequency(int[] N2, int numOfBin, int[] newArray){
-            int[] frequency = new int[numOfBin];
-            frequency[frequency.length-1] = 1;
-            for (int number: N2) {
-                for (int i = 0; i < newArray.length-1; i++) {
-                    if (number >= newArray[i] && number<newArray[i+1]){
-                        frequency[i]++;
+            int max = Arrays.stream(dataPoints).max().getAsInt();
+            int min = Arrays.stream(dataPoints).min().getAsInt();
+
+            int[] cutoffs = new int[numOfBins + 1];
+            int[] binsCount = new int[numOfBins];
+            int interval = (max - min) / numOfBins;
+            for (int j = 0; j < numOfBins + 1; j++) {
+                cutoffs[j] = min + (interval * j);
+            }
+
+            dataPointsLoop:
+            for (int data: dataPoints) {
+                for (int k = 0; k < cutoffs.length - 2; k++) {
+                    if(data < cutoffs[k + 1]){
+                        binsCount[k]++;
+                        continue dataPointsLoop;
                     }
                 }
+                if(data <= cutoffs[cutoffs.length - 1]){
+                    binsCount[numOfBins - 1]++;
+                }
             }
-            for (int freq: frequency) {
-                System.out.print(freq + " ");
-            }
+
+            System.out.println(Arrays.toString(cutoffs).replace("[", "").replace("]", "").replace(",", ""));
+            System.out.println(Arrays.toString(binsCount).replace("[", "").replace("]", "").replace(",", ""));
         }
     }
+
+}
