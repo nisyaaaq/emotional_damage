d00f0e260b9e151af46440d6e7c5d1b78c2befb4
Nazifaqf <92715392+Nazifaqf@users.noreply.github.com>
2022-06-21 13:44:17 +0800

    Update Navigation.java

--- solutions/Navigation.java
+++ solutions/Navigation.java
@@ -78,58 +78,7 @@ public class Navigation {
         }
     }
 
-    public static void BFS(UnweightedGraph x,String source, String destination){
-
-
-        String temp = "";
-        String p = "";
-        ArrayList<String> prev = new ArrayList<>();
-        String path;
-        int s = x.getSize();
-        boolean visited[] = new boolean[s];
-        LinkedList<String> que = new LinkedList<>();
-        visited[x.getIndex(source)] = true;
-        que.add(source);
-
-
-        while (que.size() != 0){
-
-
-
-            path = que.poll();
-
-
-            if(path.equals(destination)){
-                System.out.print(destination);
-                break;
-            }else{
-                for(String y : prev){
-                    if((!y.equals(temp)) )
-                    System.out.print(temp+" => "+y+" => ");
-                }
-                System.out.print(temp+" -> ");
-                prev.clear();
-            }
-
-            temp = path;
-
-                    //System.out.print(path + " -> ");
-
-            for(int i =0;i < x.getNeighbours(path).size();i++){
-
-                p = (String) x.getNeighbours(path).get(i);
-
-                prev.add(p);
-                if(!visited[x.getIndex(p)]){
-                    visited[x.getIndex(p)] = true;
-                    que.add(p);
-                }
-            }
-
-
-        }
-
-    }
+    
 
     public static int DFS(UnweightedGraph x,String source,String destination){
         boolean visited[] = new boolean[x.getSize()];
